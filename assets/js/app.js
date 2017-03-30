b71a2e60510143221faae5844a5d67b4042cf8a5
Janine Vega <osmary.vega@gmail.com>
2017-03-30 17:53:17 -0500

    listo

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+var img=document.getElementsByClassName("animal");
+var select=document.getElementById("select");
+select.onchange = function(){
+
+  if(select.value =="original"){
+    for (var i = 0; i < img.length; i++) {
+      img[i].classList.remove("sepia");
+      img[i].classList.remove("blanco-negro");
+      img[i].classList.remove("invertir-colores");
+    }
+  }
+
+  if(select.value =="sepia"){
+    for (var i = 0; i < img.length; i++) {
+      img[i].classList.remove("blanco-negro");
+      img[i].classList.remove("invertir-colores");
+      img[i].classList.add("sepia");
+    }
+  }
+
+  if(select.value =="blanco-negro"){
+    for (var i = 0; i < img.length; i++) {
+      img[i].classList.remove("sepia");
+      img[i].classList.remove("invertir-colores");
+      img[i].classList.add("blanco-negro");
+    }
+  }
+  if(select.value =="invertir-colores"){
+    for (var i = 0; i < img.length; i++) {
+      img[i].classList.remove("sepia");
+      img[i].classList.remove("blanco-negro");
+      img[i].classList.add("invertir-colores");
+    }
+  }
+}
